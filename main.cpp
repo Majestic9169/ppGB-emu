8ab48e09fdd4d6935fe096e32a93e8f31c9de4d0
Tegan Jain <teganjain527@gmail.com>
2025-03-09 10:58:03 +0530

    WIP feat(memory): add print registers method

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ class GameBoy {
 private:
   std::array<std::uint8_t, MEMORY_SIZE> MEMORY;
 
-  struct registers {
+  struct reg {
     union {
       struct {
         std::uint8_t a;
@@ -42,20 +42,46 @@ private:
     };
     std::uint16_t sp;
     std::uint16_t pc;
-  };
+  } reg;
 
 public:
   GameBoy(std::ifstream &ROM) {
+    reg.a = 0x01;
+    reg.f = 0xb0;
+    reg.b = 0x00;
+    reg.c = 0x13;
+    reg.d = 0x00;
+    reg.e = 0xd8;
+    reg.h = 0x01;
+    reg.l = 0x4d;
+    reg.sp = 0xfffe;
+    reg.pc = 0x100;
     std::cout << "[*] Loading ROM into memory from = to 0x3FFF" << std::endl;
     if (ROM.is_open()) {
       ROM.read((char *)&MEMORY, 0x4000);
       std::cout << "[+] Loaded ROM into memory" << std::endl;
     }
   }
-  void print_mem() {
-    for (int i = 0; i < 0x4000; i++) {
+  void print_mem(int n) {
+    for (int i = 0; i < n; i++) {
       printf("%5d %2x\n", i, MEMORY[i]);
     }
+  }
+  void print_reg() {
+    std::cout << "a = " << static_cast<int>(reg.a) << std::endl;
+    std::cout << "f = " << static_cast<int>(reg.f) << std::endl;
+    std::cout << "af = " << reg.af << std::endl;
+    std::cout << "b = " << static_cast<int>(reg.b) << std::endl;
+    std::cout << "c = " << static_cast<int>(reg.c) << std::endl;
+    std::cout << "bc = " << reg.bc << std::endl;
+    std::cout << "d = " << static_cast<int>(reg.d) << std::endl;
+    std::cout << "e = " << static_cast<int>(reg.e) << std::endl;
+    std::cout << "de = " << reg.de << std::endl;
+    std::cout << "h = " << static_cast<int>(reg.h) << std::endl;
+    std::cout << "l = " << static_cast<int>(reg.l) << std::endl;
+    std::cout << "hl = " << reg.hl << std::endl;
+    std::cout << "sp = " << reg.sp << std::endl;
+    std::cout << "pc = " << reg.pc << std::endl;
   }
 };
 
@@ -66,6 +92,7 @@ int main(int argc, char **argv) {
   std::ifstream ROM;
   ROM.open(argv[1], std::ios::binary | std::ios::in);
   GameBoy gb(ROM);
-  // gb.print_mem();
+  gb.print_reg();
+  gb.print_mem(0x100);
   ROM.close();
 }
