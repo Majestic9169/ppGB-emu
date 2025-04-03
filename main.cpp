d69c0a5e2e46f1f03adac726b0bf67a8e6c54835
Tegan Jain <teganjain527@gmail.com>
2025-04-03 11:09:36 +0530

    ci: disable display while building for tests

--- main.cpp
+++ main.cpp
@@ -1,27 +1,34 @@
 #include "include/cpu.h"
-#include "include/display.h"
 #include <cstring>
 #include <fstream>
 #include <ios>
 #include <iostream>
-#define DEBUG
+
+#ifndef DISABLE_DISPLAY
+#include "include/display.h"
+#endif
 
 int main(int argc, char **argv) {
   if (argc < 2) {
     std::cout << "USAGE: " << argv[0] << " <path to rom>" << std::endl;
+    return 1;
   }
+  
   std::ifstream ROM;
   ROM.open(argv[1], std::ios::binary | std::ios::in);
   CPU gb(ROM);
 
+#ifndef DISABLE_DISPLAY
   Display display(gb.framebuffer);
-
+  
   while (display.is_active()) {
+#else
+  while (true) {  // For testing without display
+#endif
 
     bool interrupted = gb.interrupt_check();
     if (!interrupted) {
       gb.INSTRUCTION_DECODER();
-      // gb.print_reg();
     }
 
     if (gb.clock_m > 0) {
@@ -33,9 +40,13 @@ int main(int argc, char **argv) {
     }
 
     gb.PPU_STEP();
+    
+#ifndef DISABLE_DISPLAY
     display.poll_event();
     display.render_frame();
+#endif
   }
 
   ROM.close();
+  return 0;
 }
