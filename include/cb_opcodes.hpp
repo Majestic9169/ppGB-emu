755ff0ab8accc09b2da7fd593cfba4b7c3585c7f
Tegan Jain <teganjain527@gmail.com>
2025-06-22 19:13:17 +0530

    feat: init cb fetch/execute cycle, pretty sure i have enough for tetris now, time to start ppu

--- include/cb_opcodes.hpp
+++ include/cb_opcodes.hpp
@@ -52,6 +52,39 @@ private:
     r8 = (r8 >> 1) | (tmp << 7);
     reg->set_z(r8);
   }
+  // SHIFT INSTRUCTIONS
+  void sla(uint8_t &val) {
+    reg->f.c = (val & 0x80) >> 7;
+    val = val << 1;
+    reg->f.n = 0;
+    reg->f.h = 0;
+    reg->set_z(val);
+  }
+  void sra(uint8_t &val) {
+    reg->f.c = (val & 0x01);
+    // c++ didn't sign extend for me, fair because unsigned char
+    val = (val >> 1) | (val & 0x80);
+    reg->f.n = 0;
+    reg->f.h = 0;
+    reg->set_z(val);
+  }
+  void srl(uint8_t &val) {
+    reg->f.c = (val & 0x01);
+    val = (val >> 1);
+    reg->f.n = 0;
+    reg->f.h = 0;
+    reg->set_z(val);
+  }
+  // SWAP INSTRUCTIONS
+  void swap(uint8_t &val) {
+    uint8_t high = (val & 0xf0);
+    uint8_t low = (val & 0x0f);
+    val = (low << 4) | (high >> 4);
+    reg->set_z(val);
+    reg->f.n = 0;
+    reg->f.h = 0;
+    reg->f.c = 0;
+  }
 
 public:
   CB_Opcodes(MMU *_mmu, Registers *_reg) : mmu{_mmu}, reg{_reg} {}
@@ -121,230 +154,262 @@ public:
   void cb_opcode_1e() { rr(mmu->read_byte(reg->hl)); };
   // RR A
   void cb_opcode_1f() { rr(reg->a); };
-  void cb_opcode_20();
-  void cb_opcode_21();
-  void cb_opcode_22();
-  void cb_opcode_23();
-  void cb_opcode_24();
-  void cb_opcode_25();
-  void cb_opcode_26();
-  void cb_opcode_27();
-  void cb_opcode_28();
-  void cb_opcode_29();
-  void cb_opcode_2a();
-  void cb_opcode_2b();
-  void cb_opcode_2c();
-  void cb_opcode_2d();
-  void cb_opcode_2e();
-  void cb_opcode_2f();
-  void cb_opcode_30();
-  void cb_opcode_31();
-  void cb_opcode_32();
-  void cb_opcode_33();
-  void cb_opcode_34();
-  void cb_opcode_35();
-  void cb_opcode_36();
-  void cb_opcode_37();
-  void cb_opcode_38();
-  void cb_opcode_39();
-  void cb_opcode_3a();
-  void cb_opcode_3b();
-  void cb_opcode_3c();
-  void cb_opcode_3d();
-  void cb_opcode_3e();
-  void cb_opcode_3f();
-  void cb_opcode_40();
-  void cb_opcode_41();
-  void cb_opcode_42();
-  void cb_opcode_43();
-  void cb_opcode_44();
-  void cb_opcode_45();
-  void cb_opcode_46();
-  void cb_opcode_47();
-  void cb_opcode_48();
-  void cb_opcode_49();
-  void cb_opcode_4a();
-  void cb_opcode_4b();
-  void cb_opcode_4c();
-  void cb_opcode_4d();
-  void cb_opcode_4e();
-  void cb_opcode_4f();
-  void cb_opcode_50();
-  void cb_opcode_51();
-  void cb_opcode_52();
-  void cb_opcode_53();
-  void cb_opcode_54();
-  void cb_opcode_55();
-  void cb_opcode_56();
-  void cb_opcode_57();
-  void cb_opcode_58();
-  void cb_opcode_59();
-  void cb_opcode_5a();
-  void cb_opcode_5b();
-  void cb_opcode_5c();
-  void cb_opcode_5d();
-  void cb_opcode_5e();
-  void cb_opcode_5f();
-  void cb_opcode_60();
-  void cb_opcode_61();
-  void cb_opcode_62();
-  void cb_opcode_63();
-  void cb_opcode_64();
-  void cb_opcode_65();
-  void cb_opcode_66();
-  void cb_opcode_67();
-  void cb_opcode_68();
-  void cb_opcode_69();
-  void cb_opcode_6a();
-  void cb_opcode_6b();
-  void cb_opcode_6c();
-  void cb_opcode_6d();
-  void cb_opcode_6e();
-  void cb_opcode_6f();
-  void cb_opcode_70();
-  void cb_opcode_71();
-  void cb_opcode_72();
-  void cb_opcode_73();
-  void cb_opcode_74();
-  void cb_opcode_75();
-  void cb_opcode_76();
-  void cb_opcode_77();
-  void cb_opcode_78();
-  void cb_opcode_79();
-  void cb_opcode_7a();
-  void cb_opcode_7b();
-  void cb_opcode_7c();
-  void cb_opcode_7d();
-  void cb_opcode_7e();
-  void cb_opcode_7f();
-  void cb_opcode_80();
-  void cb_opcode_81();
-  void cb_opcode_82();
-  void cb_opcode_83();
-  void cb_opcode_84();
-  void cb_opcode_85();
-  void cb_opcode_86();
-  void cb_opcode_87();
-  void cb_opcode_88();
-  void cb_opcode_89();
-  void cb_opcode_8a();
-  void cb_opcode_8b();
-  void cb_opcode_8c();
-  void cb_opcode_8d();
-  void cb_opcode_8e();
-  void cb_opcode_8f();
-  void cb_opcode_90();
-  void cb_opcode_91();
-  void cb_opcode_92();
-  void cb_opcode_93();
-  void cb_opcode_94();
-  void cb_opcode_95();
-  void cb_opcode_96();
-  void cb_opcode_97();
-  void cb_opcode_98();
-  void cb_opcode_99();
-  void cb_opcode_9a();
-  void cb_opcode_9b();
-  void cb_opcode_9c();
-  void cb_opcode_9d();
-  void cb_opcode_9e();
-  void cb_opcode_9f();
-  void cb_opcode_a0();
-  void cb_opcode_a1();
-  void cb_opcode_a2();
-  void cb_opcode_a3();
-  void cb_opcode_a4();
-  void cb_opcode_a5();
-  void cb_opcode_a6();
-  void cb_opcode_a7();
-  void cb_opcode_a8();
-  void cb_opcode_a9();
-  void cb_opcode_aa();
-  void cb_opcode_ab();
-  void cb_opcode_ac();
-  void cb_opcode_ad();
-  void cb_opcode_ae();
-  void cb_opcode_af();
-  void cb_opcode_b0();
-  void cb_opcode_b1();
-  void cb_opcode_b2();
-  void cb_opcode_b3();
-  void cb_opcode_b4();
-  void cb_opcode_b5();
-  void cb_opcode_b6();
-  void cb_opcode_b7();
-  void cb_opcode_b8();
-  void cb_opcode_b9();
-  void cb_opcode_ba();
-  void cb_opcode_bb();
-  void cb_opcode_bc();
-  void cb_opcode_bd();
-  void cb_opcode_be();
-  void cb_opcode_bf();
-  void cb_opcode_c0();
-  void cb_opcode_c1();
-  void cb_opcode_c2();
-  void cb_opcode_c3();
-  void cb_opcode_c4();
-  void cb_opcode_c5();
-  void cb_opcode_c6();
-  void cb_opcode_c7();
-  void cb_opcode_c8();
-  void cb_opcode_c9();
-  void cb_opcode_ca();
-  void cb_opcode_cb();
-  void cb_opcode_cc();
-  void cb_opcode_cd();
-  void cb_opcode_ce();
-  void cb_opcode_cf();
-  void cb_opcode_d0();
-  void cb_opcode_d1();
-  void cb_opcode_d2();
-  void cb_opcode_d3();
-  void cb_opcode_d4();
-  void cb_opcode_d5();
-  void cb_opcode_d6();
-  void cb_opcode_d7();
-  void cb_opcode_d8();
-  void cb_opcode_d9();
-  void cb_opcode_da();
-  void cb_opcode_db();
-  void cb_opcode_dc();
-  void cb_opcode_dd();
-  void cb_opcode_de();
-  void cb_opcode_df();
-  void cb_opcode_e0();
-  void cb_opcode_e1();
-  void cb_opcode_e2();
-  void cb_opcode_e3();
-  void cb_opcode_e4();
-  void cb_opcode_e5();
-  void cb_opcode_e6();
-  void cb_opcode_e7();
-  void cb_opcode_e8();
-  void cb_opcode_e9();
-  void cb_opcode_ea();
-  void cb_opcode_eb();
-  void cb_opcode_ec();
-  void cb_opcode_ed();
-  void cb_opcode_ee();
-  void cb_opcode_ef();
-  void cb_opcode_f0();
-  void cb_opcode_f1();
-  void cb_opcode_f2();
-  void cb_opcode_f3();
-  void cb_opcode_f4();
-  void cb_opcode_f5();
-  void cb_opcode_f6();
-  void cb_opcode_f7();
-  void cb_opcode_f8();
-  void cb_opcode_f9();
-  void cb_opcode_fa();
-  void cb_opcode_fb();
-  void cb_opcode_fc();
-  void cb_opcode_fd();
-  void cb_opcode_fe();
-  void cb_opcode_ff();
+  // SLA B
+  void cb_opcode_20() { sla(reg->b); };
+  // SLA C
+  void cb_opcode_21() { sla(reg->c); };
+  // SLA D
+  void cb_opcode_22() { sla(reg->d); };
+  // SLA E
+  void cb_opcode_23() { sla(reg->e); };
+  // SLA H
+  void cb_opcode_24() { sla(reg->h); };
+  // SLA L
+  void cb_opcode_25() { sla(reg->l); };
+  // SLA (HL)
+  void cb_opcode_26() { sla(mmu->read_byte(reg->hl)); };
+  // SLA A
+  void cb_opcode_27() { sla(reg->a); };
+  // SRA B
+  void cb_opcode_28() { sra(reg->b); };
+  // SRA C
+  void cb_opcode_29() { sra(reg->c); };
+  // SRA D
+  void cb_opcode_2a() { sra(reg->d); };
+  // SRA E
+  void cb_opcode_2b() { sra(reg->e); };
+  // SRA H
+  void cb_opcode_2c() { sra(reg->h); };
+  // SRA L
+  void cb_opcode_2d() { sra(reg->l); };
+  // SRA (HL)
+  void cb_opcode_2e() { sra(mmu->read_byte(reg->hl)); };
+  // SRA A
+  void cb_opcode_2f() { sra(reg->a); };
+  // SWAP B
+  void cb_opcode_30() { swap(reg->b); };
+  // SWAP C
+  void cb_opcode_31() { swap(reg->c); };
+  // SWAP D
+  void cb_opcode_32() { swap(reg->d); };
+  // SWAP E
+  void cb_opcode_33() { swap(reg->e); };
+  // SWAP H
+  void cb_opcode_34() { swap(reg->h); };
+  // SWAP L
+  void cb_opcode_35() { swap(reg->l); };
+  // SWAP (HL)
+  void cb_opcode_36() { swap(mmu->read_byte(reg->hl)); };
+  // SWAP A
+  void cb_opcode_37() { swap(reg->a); };
+  // SRL B
+  void cb_opcode_38() { srl(reg->b); };
+  // SRL C
+  void cb_opcode_39() { srl(reg->c); };
+  // SRL D
+  void cb_opcode_3a() { srl(reg->d); };
+  // SRL E
+  void cb_opcode_3b() { srl(reg->e); };
+  // SRL H
+  void cb_opcode_3c() { srl(reg->h); };
+  // SRL L
+  void cb_opcode_3d() { srl(reg->l); };
+  // SRL (HL)
+  void cb_opcode_3e() { srl(mmu->read_byte(reg->hl)); };
+  // SRL A
+  void cb_opcode_3f() { srl(reg->a); };
+  // void cb_opcode_40();
+  // void cb_opcode_41();
+  // void cb_opcode_42();
+  // void cb_opcode_43();
+  // void cb_opcode_44();
+  // void cb_opcode_45();
+  // void cb_opcode_46();
+  // void cb_opcode_47();
+  // void cb_opcode_48();
+  // void cb_opcode_49();
+  // void cb_opcode_4a();
+  // void cb_opcode_4b();
+  // void cb_opcode_4c();
+  // void cb_opcode_4d();
+  // void cb_opcode_4e();
+  // void cb_opcode_4f();
+  // void cb_opcode_50();
+  // void cb_opcode_51();
+  // void cb_opcode_52();
+  // void cb_opcode_53();
+  // void cb_opcode_54();
+  // void cb_opcode_55();
+  // void cb_opcode_56();
+  // void cb_opcode_57();
+  // void cb_opcode_58();
+  // void cb_opcode_59();
+  // void cb_opcode_5a();
+  // void cb_opcode_5b();
+  // void cb_opcode_5c();
+  // void cb_opcode_5d();
+  // void cb_opcode_5e();
+  // void cb_opcode_5f();
+  // void cb_opcode_60();
+  // void cb_opcode_61();
+  // void cb_opcode_62();
+  // void cb_opcode_63();
+  // void cb_opcode_64();
+  // void cb_opcode_65();
+  // void cb_opcode_66();
+  // void cb_opcode_67();
+  // void cb_opcode_68();
+  // void cb_opcode_69();
+  // void cb_opcode_6a();
+  // void cb_opcode_6b();
+  // void cb_opcode_6c();
+  // void cb_opcode_6d();
+  // void cb_opcode_6e();
+  // void cb_opcode_6f();
+  // void cb_opcode_70();
+  // void cb_opcode_71();
+  // void cb_opcode_72();
+  // void cb_opcode_73();
+  // void cb_opcode_74();
+  // void cb_opcode_75();
+  // void cb_opcode_76();
+  // void cb_opcode_77();
+  // void cb_opcode_78();
+  // void cb_opcode_79();
+  // void cb_opcode_7a();
+  // void cb_opcode_7b();
+  // void cb_opcode_7c();
+  // void cb_opcode_7d();
+  // void cb_opcode_7e();
+  // void cb_opcode_7f();
+  // void cb_opcode_80();
+  // void cb_opcode_81();
+  // void cb_opcode_82();
+  // void cb_opcode_83();
+  // void cb_opcode_84();
+  // void cb_opcode_85();
+  // void cb_opcode_86();
+  // void cb_opcode_87();
+  // void cb_opcode_88();
+  // void cb_opcode_89();
+  // void cb_opcode_8a();
+  // void cb_opcode_8b();
+  // void cb_opcode_8c();
+  // void cb_opcode_8d();
+  // void cb_opcode_8e();
+  // void cb_opcode_8f();
+  // void cb_opcode_90();
+  // void cb_opcode_91();
+  // void cb_opcode_92();
+  // void cb_opcode_93();
+  // void cb_opcode_94();
+  // void cb_opcode_95();
+  // void cb_opcode_96();
+  // void cb_opcode_97();
+  // void cb_opcode_98();
+  // void cb_opcode_99();
+  // void cb_opcode_9a();
+  // void cb_opcode_9b();
+  // void cb_opcode_9c();
+  // void cb_opcode_9d();
+  // void cb_opcode_9e();
+  // void cb_opcode_9f();
+  // void cb_opcode_a0();
+  // void cb_opcode_a1();
+  // void cb_opcode_a2();
+  // void cb_opcode_a3();
+  // void cb_opcode_a4();
+  // void cb_opcode_a5();
+  // void cb_opcode_a6();
+  // void cb_opcode_a7();
+  // void cb_opcode_a8();
+  // void cb_opcode_a9();
+  // void cb_opcode_aa();
+  // void cb_opcode_ab();
+  // void cb_opcode_ac();
+  // void cb_opcode_ad();
+  // void cb_opcode_ae();
+  // void cb_opcode_af();
+  // void cb_opcode_b0();
+  // void cb_opcode_b1();
+  // void cb_opcode_b2();
+  // void cb_opcode_b3();
+  // void cb_opcode_b4();
+  // void cb_opcode_b5();
+  // void cb_opcode_b6();
+  // void cb_opcode_b7();
+  // void cb_opcode_b8();
+  // void cb_opcode_b9();
+  // void cb_opcode_ba();
+  // void cb_opcode_bb();
+  // void cb_opcode_bc();
+  // void cb_opcode_bd();
+  // void cb_opcode_be();
+  // void cb_opcode_bf();
+  // void cb_opcode_c0();
+  // void cb_opcode_c1();
+  // void cb_opcode_c2();
+  // void cb_opcode_c3();
+  // void cb_opcode_c4();
+  // void cb_opcode_c5();
+  // void cb_opcode_c6();
+  // void cb_opcode_c7();
+  // void cb_opcode_c8();
+  // void cb_opcode_c9();
+  // void cb_opcode_ca();
+  // void cb_opcode_cb();
+  // void cb_opcode_cc();
+  // void cb_opcode_cd();
+  // void cb_opcode_ce();
+  // void cb_opcode_cf();
+  // void cb_opcode_d0();
+  // void cb_opcode_d1();
+  // void cb_opcode_d2();
+  // void cb_opcode_d3();
+  // void cb_opcode_d4();
+  // void cb_opcode_d5();
+  // void cb_opcode_d6();
+  // void cb_opcode_d7();
+  // void cb_opcode_d8();
+  // void cb_opcode_d9();
+  // void cb_opcode_da();
+  // void cb_opcode_db();
+  // void cb_opcode_dc();
+  // void cb_opcode_dd();
+  // void cb_opcode_de();
+  // void cb_opcode_df();
+  // void cb_opcode_e0();
+  // void cb_opcode_e1();
+  // void cb_opcode_e2();
+  // void cb_opcode_e3();
+  // void cb_opcode_e4();
+  // void cb_opcode_e5();
+  // void cb_opcode_e6();
+  // void cb_opcode_e7();
+  // void cb_opcode_e8();
+  // void cb_opcode_e9();
+  // void cb_opcode_ea();
+  // void cb_opcode_eb();
+  // void cb_opcode_ec();
+  // void cb_opcode_ed();
+  // void cb_opcode_ee();
+  // void cb_opcode_ef();
+  // void cb_opcode_f0();
+  // void cb_opcode_f1();
+  // void cb_opcode_f2();
+  // void cb_opcode_f3();
+  // void cb_opcode_f4();
+  // void cb_opcode_f5();
+  // void cb_opcode_f6();
+  // void cb_opcode_f7();
+  // void cb_opcode_f8();
+  // void cb_opcode_f9();
+  // void cb_opcode_fa();
+  // void cb_opcode_fb();
+  // void cb_opcode_fc();
+  // void cb_opcode_fd();
+  // void cb_opcode_fe();
+  // void cb_opcode_ff();
 };
 
 #endif
--- tests/cb_inst_test.cpp
+++ tests/cb_inst_test.cpp
@@ -107,3 +107,96 @@ TEST_CASE("RL tests", "[flags, rl]") {
   REQUIRE(reg.f.n == 0);
   REQUIRE(reg.f.c == 0);
 }
+
+TEST_CASE("SLA tests", "[flags, sla]") {
+  reg.af = 0xfff0;
+  reg.b = 0x40;
+  cb.cb_opcode_20();
+  REQUIRE(reg.b == 0x80);
+  REQUIRE(reg.f.z == 0);
+  REQUIRE(reg.f.h == 0);
+  REQUIRE(reg.f.n == 0);
+  REQUIRE(reg.f.c == 0);
+  reg.c = 0xff;
+  cb.cb_opcode_21();
+  REQUIRE(reg.c == 0xfe);
+  REQUIRE(reg.f.z == 0);
+  REQUIRE(reg.f.h == 0);
+  REQUIRE(reg.f.n == 0);
+  REQUIRE(reg.f.c == 1);
+  reg.hl = 0x8080;
+  mmu.write_byte(reg.hl, 0x80);
+  cb.cb_opcode_26();
+  REQUIRE(mmu.read_byte(reg.hl) == 0x00);
+  REQUIRE(reg.f.z == 1);
+  REQUIRE(reg.f.h == 0);
+  REQUIRE(reg.f.n == 0);
+  REQUIRE(reg.f.c == 1);
+}
+
+TEST_CASE("SRA tests", "[flags, sra]") {
+  reg.af = 0xfff0;
+  reg.b = 0x40;
+  cb.cb_opcode_28();
+  REQUIRE(reg.b == 0x20);
+  REQUIRE(reg.f.z == 0);
+  REQUIRE(reg.f.h == 0);
+  REQUIRE(reg.f.n == 0);
+  REQUIRE(reg.f.c == 0);
+  reg.c = 0xff;
+  cb.cb_opcode_29();
+  REQUIRE(reg.c == 0xff);
+  REQUIRE(reg.f.z == 0);
+  REQUIRE(reg.f.h == 0);
+  REQUIRE(reg.f.n == 0);
+  REQUIRE(reg.f.c == 1);
+  reg.hl = 0x8080;
+  mmu.write_byte(reg.hl, 0x80);
+  cb.cb_opcode_2e();
+  REQUIRE(mmu.read_byte(reg.hl) == 0xc0);
+  REQUIRE(reg.f.z == 0);
+  REQUIRE(reg.f.h == 0);
+  REQUIRE(reg.f.n == 0);
+  REQUIRE(reg.f.c == 0);
+}
+
+TEST_CASE("SWAP tests", "[flags, swap]") {
+  reg.af = 0xfff0;
+  reg.b = 0x40;
+  cb.cb_opcode_30();
+  REQUIRE(reg.b == 0x04);
+  REQUIRE(reg.f.z == 0);
+  REQUIRE(reg.f.h == 0);
+  REQUIRE(reg.f.n == 0);
+  REQUIRE(reg.f.c == 0);
+  reg.c = 0xff;
+  cb.cb_opcode_31();
+  REQUIRE(reg.c == 0xff);
+  REQUIRE(reg.f.z == 0);
+  REQUIRE(reg.f.h == 0);
+  REQUIRE(reg.f.n == 0);
+  REQUIRE(reg.f.c == 0);
+  reg.hl = 0x8080;
+  mmu.write_byte(reg.hl, 0xfe);
+  cb.cb_opcode_36();
+  REQUIRE(mmu.read_byte(reg.hl) == 0xef);
+  REQUIRE(reg.f.z == 0);
+  REQUIRE(reg.f.h == 0);
+  REQUIRE(reg.f.n == 0);
+  REQUIRE(reg.f.c == 0);
+  reg.d = 0xf1;
+  cb.cb_opcode_32();
+  REQUIRE(reg.d == 0x1f);
+  REQUIRE(reg.f.z == 0);
+  REQUIRE(reg.f.h == 0);
+  REQUIRE(reg.f.n == 0);
+  REQUIRE(reg.f.c == 0);
+  reg.h = 0x00;
+  cb.cb_opcode_34();
+  REQUIRE(reg.h == 0x00);
+  REQUIRE(reg.f.z == 1);
+  REQUIRE(reg.f.h == 0);
+  REQUIRE(reg.f.n == 0);
+  REQUIRE(reg.f.c == 0);
+  REQUIRE(reg.hl == 0x0080);
+}
