a6a37fdeb34ab5e32fbfb1472978bea65c6b037c
DarkGuy10 <SSSCerealSoup@proton.me>
2025-10-15 18:28:53 +0530

    chore: add basic nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import (inputs.nixpkgs) { inherit system; });
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cmake
+            SDL2
+            catch2_3
+            nlohmann_json
+            clang-tools
+          ];
+        };
+      }
+    );
+}
